c363a547673252b8b14c4acf1aeb9626fc9581fe
LostAxeHead2Kings6 <mvasios1@gmail.com>
2018-02-21 11:25:08 -0500

    description Card changes on click

--- server/index.js
+++ server/index.js
@@ -26,7 +26,6 @@ app.get("/fetch", function(req, res) {
 //This route receives a request upon submit from the form. The form holds all fields necesaary
 //to make a new db entry. This route will take in the request and simply save to the db
 app.post("/savepost", function(req, res) {
-  console.log('HELLO', req.body);
   var listing = req.body
   console.log("saving to database...");
   db.query(
@@ -37,7 +36,7 @@ app.post("/savepost", function(req, res) {
       listing.isClaimed
     });`,
     (err, data) => {
-      if (err) console.log("Error saving to database", err);
+      if (err) console.log("Error saving to database", error);
       console.log("succesfully saved to database");
       res.end();
     }
@@ -48,3 +47,10 @@ var _PORT = process.env.PORT || 3000;
 app.listen(_PORT, function() {
   console.log("listening on port 3000!");
 });
+
+
+  // db.query("SELECT * FROM users", (err, results) => {
+  //   if (err) console.log("FAILED to retrieve from database");
+  //   else {
+  //     if results.includes(req.body) 
+  //   }

c4110e42bec5fdd35589ca473fc23d95bd5764c9
quietmind <adammateo@gmail.com>
2018-02-21 17:27:01 -0500

    conditional rendering update

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,7 +5,6 @@ import Form from "./components/form.jsx";
 import DescriptionCard from "./components/descriptionCard.jsx";
 import axios from "axios";
 // import AnyComponent from './components/filename.jsx'
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -15,26 +14,25 @@ class App extends React.Component {
         title: null,
         description: null,
         id: null
-      }
+      },
+      show: false
     };
-
     this.retrievePosts = this.retrievePosts.bind(this);
     this.savePosts = this.savePosts.bind(this);
     this.changeFeatured = this.changeFeatured.bind(this);
     this.handleClaim = this.handleClaim.bind(this);
     this.resetFormView = this.handleClaim.bind(this);
   }
-
   componentDidMount() {
     this.retrievePosts();
     this.savePosts();
     console.log("List Items:", this.state.posts);
   }
-
   changeFeatured(listItem) {
-    this.setState({ featuredItem: listItem });
+    this.setState({ featuredItem: listItem,
+                    show: !this.state.show
+     });
   }
-
   retrievePosts() {
     axios
       .get("/fetch")
@@ -45,15 +43,12 @@ class App extends React.Component {
         console.log("There was an error retrieving posts.", error);
       });
   }
-
   savePosts(data) {
     this.setState({
       posts: data
     });
     console.log("this is state.posts: ", this.state.posts);
   }
-
-
   handleClaim(claimedPostID) {
     console.log("claimedPost clicked", claimedPostID);
     axios
@@ -64,7 +59,6 @@ class App extends React.Component {
         this.retrievePosts();
       });
   }
-
   render() {
     return (
       <div>
@@ -78,7 +72,7 @@ class App extends React.Component {
               />
             </ReactBootstrap.Col>
             <ReactBootstrap.Col className="pass" md={6}>
-             {this.state.featuredItem.title === null ? <Form /> :  <DescriptionCard
+             {this.state.show === false ? <Form /> :  <DescriptionCard
                 title={this.state.featuredItem.title}
                 description={this.state.featuredItem.description}
                 id={this.state.featuredItem.id}
@@ -92,3 +86,6 @@ class App extends React.Component {
   }
 }
 ReactDOM.render(<App />, document.getElementById("app"));
+
+
+Add Comment

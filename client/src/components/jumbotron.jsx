bbf0d8927b9bfd5c716870f756a6ea081ffd19a1
aloralyn <aloralyn@gmail.com>
2018-02-22 15:41:13 -0500

    Jumbotron
    - Add a jumbotron with photo

---
+++ client/src/components/jumbotron.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Jumbotron, Button} from 'react-bootstrap';
+
+const Maintron = (props) => {
+  return (
+    <Jumbotron >
+      <h1>Support your community today.</h1>
+      <p>
+        ENTER SOME WORDS OF ENCOURAGEMENT HERE AND
+        NOTE SURE WHERE THE BUTTON SHOULD LEAD TO.
+      </p>
+      <p>
+        <Button bsStyle="primary">Learn more</Button>
+      </p>
+    </Jumbotron>
+  )
+
+}
+
+export default Maintron

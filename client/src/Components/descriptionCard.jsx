b3fa0e7029b0629dd1f3725e608c29f4c8ae0cc0
Yu Qing Dong <yu_qing630@yahoo.com>
2018-02-22 10:53:05 -0500

    setting up email in description card

--- client/src/Components/descriptionCard.jsx
+++ client/src/Components/descriptionCard.jsx
@@ -3,11 +3,12 @@ import moment from 'moment';
 const DescriptionCard = (props) => (
       <div style={{backgroundColor:'#1574cd', borderRadius:'5px'}}>
         <h2>{props.featuredItem.title}</h2>
+        <p>{props.featuredItem.city}, {props.featuredItem.state} {props.featuredItem.address}</p>
         <p>posted on:{moment(props.featuredItem.createdAt).format('MMMM Do YYYY, h:mm:ss a')}</p>
         <p>{props.featuredItem.description}</p>
-        <p>{props.featuredItem.city}, {props.featuredItem.state} {props.featuredItem.address}</p>
+        <p>Reach me at: {props.featuredItem.emailAddress}</p>
         <button onClick={() => props.claimHandler(props.featuredItem.id)}>Claim</button>
       </div>
-  
+
 )
 export default DescriptionCard;

c4348e3819661c41ccfe936bc65a0fe679e39ac4
Yu Qing Dong <yu_qing630@yahoo.com>
2018-02-22 11:18:04 -0500

    fixing the description card date

--- client/src/Components/descriptionCard.jsx
+++ client/src/Components/descriptionCard.jsx
@@ -4,7 +4,7 @@ const DescriptionCard = (props) => (
       <div style={{backgroundColor:'#1574cd', borderRadius:'5px'}}>
         <h2>{props.featuredItem.title}</h2>
         <p>{props.featuredItem.city}, {props.featuredItem.state} {props.featuredItem.address}</p>
-        <p>posted on:{moment(props.featuredItem.createdAt).format('MMMM Do YYYY, h:mm:ss a')}</p>
+        <p>posted on:{moment(parseInt(props.featuredItem.createdAt)).format('MMMM Do YYYY, h:mm:ss a')}</p>
         <p>{props.featuredItem.description}</p>
         <p>Reach me at: {props.featuredItem.emailAddress}</p>
         <button onClick={() => props.claimHandler(props.featuredItem.id)}>Claim</button>

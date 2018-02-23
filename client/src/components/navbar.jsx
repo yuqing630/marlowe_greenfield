64bc21adce349a9e05f9d97cea110e5b054f5bf4
Yu Qing Dong <yu_qing630@yahoo.com>
2018-02-23 14:59:52 -0500

    style nav bar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -16,15 +16,12 @@ class NavigationBar extends React.Component {
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
-            <a href="#home">Our Fantastical Greenfield Project</a>
+            <a href="#home">Kindly</a>
           </Navbar.Brand>
         </Navbar.Header>
         <Nav>
           <NavItem eventKey={1} href="#" onClick={() => props.scrollTo()}>
             Recent Posts
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            Your Claim Posts
           </NavItem>
         </Nav>
         <Nav pullRight>
